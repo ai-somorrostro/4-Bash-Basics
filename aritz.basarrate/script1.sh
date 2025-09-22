fb275d7c0ac44c3614bf32f4caa7a602fd5601e4
aritz.basarrate <aritz.basarrate05@somo.eus>
2025-09-22 08:44:42 +0200

    creando carpeta

---
+++ aritz.basarrate/script1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#EJERCICIO1:Tu Primer Script Bash - Saludando al Mundo
+#Conceptos Clave: Creación de un script, comando echo, permisos de ejecución,el shebang.
+#Escenario: Crea un script Bash que imprima un saludo amistoso en la terminal.
+
+#echo muestra texto en la pantalla
+echo "¡Hola, Estudiantes de Bash!"
