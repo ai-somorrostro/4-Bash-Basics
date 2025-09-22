7c61358085099c150fd0802399dabb7d132de030
Aimar Sarachaga <aimar.sarachaga02@somo.eus>
2025-09-22 09:07:08 +0200

    script3.sh corregido error

--- aimar.sarachaga/script3.sh
+++ aimar.sarachaga/script3.sh
@@ -4,4 +4,4 @@
 # Crea o sobrescribe el archivo mis_notas.txt y añade el texto.
 echo "Hoy aprendí a crear archivos con Bash." > mis_notas.txt
 
-echo "Archivo 'mis_notas.txt' creado con el texto."```
+echo "Archivo 'mis_notas.txt' creado con el texto."
