8e13b4b8435f762bd77dd122cdad13349cb4abf9
Iker Gorostiaga <iker.gorostiaga04@somo.eus>
2025-09-22 08:21:22 +0200

    creados los archivos script2 y 3

---
+++ iker.gorostiaga/script2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# pedimos nombre y lo guardamos en la variable nombre
+echo "Cuentame, cual es tu nombre?"
+read nombre
+
+echo "Hola, $nombre!"
+
---
+++ iker.gorostiaga/script3.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Crear el archivo y escribir el texto dentro(se sobreescribira si ya existe
+#(usar >> para agregarlo y no sobreescribirlo))
+echo "Hoy aprendí a crear archivos con Bash." > mis_notas.txt
