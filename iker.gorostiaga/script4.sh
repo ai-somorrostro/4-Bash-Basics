f7196000b3bc0c37596ffad7c06fcadd66788be9
Iker Gorostiaga <iker.gorostiaga04@somo.eus>
2025-09-22 08:29:40 +0200

    creado script 4 y ejecutados script 4 y 3

---
+++ iker.gorostiaga/script4.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#NO VERIFICA SI EXITE O NO EL TXT ANTES DE MOVERLO
+# Crear una carpeta llamada .documentos.
+mkdir documentos
+
+# Mover el archivo .mis_notas.txt. a la carpeta .documentos.
+mv mis_notas.txt documentos/
